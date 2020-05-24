dc471be372bfffa6a5e49a4522c8ad1bc0eba8e8
pazursm <pazur_s@yahoo.com>
2020-05-24 08:54:39 +0200

    Add project files.

---
+++ SimpleMvcCRUDApp/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SimpleMvcCRUDApp.Models;
+
+namespace SimpleMvcCRUDApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //GET
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.Products.ToListAsync());
+        }
+    }
+}
